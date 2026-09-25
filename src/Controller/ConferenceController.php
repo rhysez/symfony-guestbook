70f69c5da477f2476e67f8166d721cc7a2049fae
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-09-25 11:44:21 +0100

    refactor: Ensure ConferenceController uses $this->render

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -11,32 +11,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
-use Twig\Environment;
 
 final class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response($twig->render('conference/index.html.twig', [
+        return $this->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
-        ]));
+        ]);
     }
 
     #[Route('/conference/{id}', name: 'conference')]
     public function show(
-        Environment $twig,
         #[MapEntity] Conference $conference,
         CommentRepository $commentRepository,
         #[MapQueryParameter(options: ['min_range' => 0])] int $offset = 0): Response
     {
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
-        return new Response($twig->render('conference/show.html.twig', [
+        return $this->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE)
-        ]));
+        ]);
     }
 }
