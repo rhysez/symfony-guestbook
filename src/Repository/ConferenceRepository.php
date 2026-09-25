be7c323039af73e5f9600f60dea996d882e80d0a
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-09-25 12:27:40 +0100

    feat: Ensure comment created_at is set on pre-persist

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -16,6 +16,11 @@ class ConferenceRepository extends ServiceEntityRepository
         parent::__construct($registry, Conference::class);
     }
 
+    public function findAll(): array
+    {
+        return $this->findBy(criteria:[], orderBy: ['year' => 'ASC', 'city' => 'ASC']);
+    }
+
 //    /**
 //     * @return Conference[] Returns an array of Conference objects
 //     */
