dbeb0b6bf1a00739a7d41adee6ce8bf7a1faf7e6
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-09-25 14:20:55 +0100

    refactor: Ensure conference routes use slugs instead of ids

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -22,9 +22,9 @@ final class ConferenceController extends AbstractController
         ]);
     }
 
-    #[Route('/conference/{id}', name: 'conference')]
+    #[Route('/conference/{slug:conference}', name: 'conference')]
     public function show(
-        #[MapEntity] Conference $conference,
+        Conference $conference,
         CommentRepository $commentRepository,
         #[MapQueryParameter(options: ['min_range' => 0])] int $offset = 0): Response
     {
--- src/EntityListener/ConferenceEntityListener.php
+++ src/EntityListener/ConferenceEntityListener.php
@@ -3,10 +3,14 @@
 namespace App\EntityListener;
 
 use App\Entity\Conference;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Events;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
+#[AsEntityListener(event: Events::prePersist, entity: Conference::class)]
+#[AsEntityListener(event: Events::preUpdate, entity: Conference::class)]
 class ConferenceEntityListener
 {
     public function __construct(
