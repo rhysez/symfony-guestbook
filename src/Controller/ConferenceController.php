9bea70f4d118ef1df445cd046e6a485751381e70
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-09-25 14:45:49 +0100

    feat: WIP

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ){}
+
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
@@ -24,17 +31,32 @@ final class ConferenceController extends AbstractController
 
     #[Route('/conference/{slug:conference}', name: 'conference')]
     public function show(
+        Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
         #[MapQueryParameter(options: ['min_range' => 0])] int $offset = 0): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Attach the comment to the current conference.
+            $comment->setConference($conference);
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return $this->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
-            'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE)
+            'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE),
+            'comment_form' => $form
         ]);
     }
 }
